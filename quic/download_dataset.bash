86801303fa3cdf930668366cee9f352448db0ad8
Piet De Vaere <piet@devae.re>
2018-10-11 13:59:41 +0200

    made downloadscript more indirect

--- quic/download_dataset.bash
+++ quic/download_dataset.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-DATASET_URL="http://muninn.ethz.ch/spinbit/for_eth_archive/quic_data.tar.gz"
+DOWNLOAD_SCRIPT_URL="https://raw.githubusercontent.com/mami-project/three-bits-suffice/master/quic/.download_magic/download_dataset.bash"
 
-read -p "Download dataset? This will use about 100 GB. (y/N) " -n 1 -r
-echo    # (optional) move to a new line
+echo "I will fetch and run the following script:"
+echo $DOWNLOAD_SCRIPT_URL
+read -p "Continue? (y/N) " -n 1 -r
+echo  # newline
 if [[ $REPLY =~ ^[Yy]$ ]]
-then
-	curl $DATASET_URL | tar -xvzf - 
-    fi
+	then
+		source <(curl -v $DOWNLOAD_SCRIPT_URL)
+fi
