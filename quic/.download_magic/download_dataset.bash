357e68298e8e12d8a81e0a4ae51f82bc7d07390b
Piet De Vaere <piet@devae.re>
2018-10-11 19:15:24 +0200

    pointed to files in ETH library archive

--- quic/.download_magic/download_dataset.bash
+++ quic/.download_magic/download_dataset.bash
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-DATASET_URL="http://muninn.ethz.ch/spinbit/for_eth_archive/quic_data.tar.gz"
+DATASET_URLS=(\
+	"https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/294813/quic_dataset_part_1.tar"\
+	"https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/294813/quic_dataset_part_2.tar"\
+	"https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/294813/quic_dataset_part_3.tar"\
+	"https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/294813/quic_dataset_part_4.tar"\
+	"https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/294813/quic_dataset_part_5.tar"\
+	"https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/294813/quic_dataset_part_6.tar"\
+)
 
-read -p "Download dataset? This will use about 100 GB. (y/N) " -n 1 -r
+read -p "Download dataset? This will use about 80 GB. (y/N) " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+	for URL in "${DATASET_URLS[@]}"
 	rm -rf data
-	curl $DATASET_URL | tar -xvzf - 
-    fi
+	do
+		echo $URL
+		curl $URL | tar -xvf - -C data
+	done 
+fi
